8a28755eb37a09cc4b96d963466822339e370882
Gerant <geraskin.antony@yandex.ru>
2021-01-16 15:31:07 +0700

    1)создал мавен проект. 2) написал тест на гет запрос

---
+++ src/test/java/DemoGetRequest.java
@@ -0,0 +1,29 @@
+/*
+given()
+    set cookies, add auth, add param, set headers info etc...
+when()
+    get, post, put, delete...
+then()
+    validate status code, extract response, extract headers cookies & response body..
+
+ */
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class DemoGetRequest {
+
+    @Test
+    public void Demo1_GET_Request(){
+            given()
+            .when()
+                .get("https://jsonplaceholder.typicode.com/posts/1")
+            .then()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .assertThat().body("userId", equalTo(1))
+                .header("Content-Type","application/json; charset=utf-8");
+
+    }
+
+}
